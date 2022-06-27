4bf7620c2687108629166a9c7fd1a6dddf9b88bb
viniciusgasparino <viniciusgasparino@yahoo.com.br>
2022-06-27 19:37:03 -0300

    Criado projeto exercicios de Php em mvc

---
+++ app/controllers/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\controllers;
+
+    class HomeController{
+        
+        public function index(){
+            echo "<br>Controller padrão<br>";                
+        } 
+    }
+                
+
+
+ 
+?>    
